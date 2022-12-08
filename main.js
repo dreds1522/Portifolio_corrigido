5b07f1e3b3534036d053a7e38839b6c64813522d
Andre Sampaio <andresampaio37@outlook.com>
2022-12-08 19:32:13 -0300

    Ajustes responsividade

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*Ajuste botão menu*/
- $(document).ready(function(){
-    $(".btn-mobile").click(function(){
+$(document).ready(function () {
+    $(".btn-mobile").click(function () {
         $(".menu-mobile").slideToggle("slow")
     })
 })
@@ -8,33 +8,33 @@
 /* Modal-1 */
 const modal = document.querySelector('.modal-container')
 
-function openModal(){
+function openModal() {
     modal.classList.add('active')
 }
 
-function closeModal(){
+function closeModal() {
     modal.classList.remove('active')
 }
 
 /* Modal-2 */
 const modal2 = document.querySelector('.modal-container2')
 
-function openModal2(){
+function openModal2() {
     modal2.classList.add('active')
 }
 
-function closeModal2(){
+function closeModal2() {
     modal2.classList.remove('active')
 }
 
 /* Modal-3 */
 const modal3 = document.querySelector('.modal-container3')
 
-function openModal3(){
+function openModal3() {
     modal3.classList.add('active')
 }
 
-function closeModal3(){
+function closeModal3() {
     modal3.classList.remove('active')
 }
 
@@ -42,21 +42,41 @@ function closeModal3(){
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('nav .menu ul li');
 
-window.addEventListener('scroll',()=> {
+window.addEventListener('scroll', () => {
     let current = '';
-    
 
-    sections.forEach( section => {
+
+    sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if(pageYOffset >= (sectionTop - sectionHeight / 3)){
+        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
             current = section.getAttribute('id');
         }
     })
     navLi.forEach(li => {
         li.classList.remove('active');
-        if(li.classList.contains(current)){
+        if (li.classList.contains(current)) {
             li.classList.add('active')
         }
     })
 })
+
+
+
+let inputNome = document.querySelector('#inputNome')
+let inputEmail = document.querySelector('#inputEmail')
+let textMensagem = document.querySelector('#textMensagem')
+
+function cadastrar() {
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+
+    listaUser.push(
+    {
+        nome: inputNome.value,
+        email: inputEmail.value,
+        mensagem: textMensagem.value
+    }    
+    )
+
+    localStorage.setItem('listaUser', JSON.stringify(listaUser))
+}
